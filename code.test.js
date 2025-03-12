0c3244a3caca60e811fa53f80037447b0dbc7af1
noahcanen <ncanen@uwyo.edu>
2025-03-12 15:46:46 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -7,8 +7,7 @@ const testSort =
     jsc.forall("array nat", function(arr) {
         var a1 = JSON.parse(JSON.stringify(arr));
         var a2 = JSON.parse(JSON.stringify(arr));
-        return JSON.stringify(mergesort(a1)) ==
-            JSON.stringify(a2.sort());
+        return JSON.stringify(mergesort(a1)) ==  JSON.stringify(a2.sort(function(a, b){ return a - b; }));
     });
 
 jsc.assert(testSort);
