1db43e2d1c2b4bc43cf07f4cf8801ca577197a06
noahcanen <ncanen@uwyo.edu>
2025-03-12 15:32:59 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,8 @@
 function mergesort(array) {
-    
+
     let hi = array.length;
-    for (var i = 1; i < hi; i=i*2)
-    {
+    for (var i = 1; i < hi; i=i*2){
         for(var k = 0; k <= hi; k= k + i*2) {
-            
             for(var s1 = k; s1 < k + i*2;s1++){
                 for(var s2 = k; s2 < k + i*2;s2++){
                     if(x[s1]  < x[s2]) {
@@ -16,7 +14,6 @@ function mergesort(array) {
                 }
             }
         }
-
     }
     return array;
 }
