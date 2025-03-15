2eeab338027ee76b621ff897c0709c8e3799fbc6
noahcanen <ncanen@uwyo.edu>
2025-03-14 18:30:59 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function mergesort(x) {
     let hi = x.length;
 
      for (var i = 1; i < hi; i= i*2){
-         for(var k = 0; k <= hi; k = k + i*2) {
+         for(var k = 0; k < hi; k = k + i*2) {
              for(var s1 = k; s1 < k + i*2;s1++){
                  for(var s2 = k; s2 < k + i*2;s2++){
                      if(x[s1]  < x[s2]) {
